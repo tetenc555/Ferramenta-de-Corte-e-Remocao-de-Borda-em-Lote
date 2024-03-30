df05a2b43d0d40c89fe6fbff0add717c644b8d1d
Estêvão Fiorilo <estevaofiorilo@gmail.com>
2024-03-30 19:59:08 -0300

    apaga arquivos de foto inteiros com apenas a borda removida

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.corte_lote import maincorte
 from src.bdremove import mainbdremove
+import os
 
 ##dirinicial=str(input("Digite o nome da pasta em que as fotos estão localizadas: ")) ##nao utilizado pois sera usada a pasta padrão input
 porcentagem = int(input("Digite a porcentagem de borda que deseja remover: "))
@@ -9,3 +10,6 @@ altura_resolucao = int(input("Digite a altura da resolução desejada: "))
 ##mainbdremove(dirinicial,porcentagem) ##nao utilizado pois sera usada a pasta padrão input
 mainbdremove('input',porcentagem)
 maincorte(largura_resolucao, altura_resolucao)
+for arq in os.listdir('outputBD'):
+    os.remove(f'outputBD/{arq}')
+os.rmdir('outputBD')
