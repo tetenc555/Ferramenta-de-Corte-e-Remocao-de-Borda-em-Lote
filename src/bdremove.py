53566765ab58d30a2903f63b8f3c2c4cc4f6a651
Estêvão Fiorilo <estevaofiorilo@gmail.com>
2024-03-30 20:12:27 -0300

    atualiza comentarios

--- src/bdremove.py
+++ src/bdremove.py
@@ -1,37 +1,38 @@
 import os
 from PIL import Image
 def removeborda(imagem, porcentagem):
-    ##define img como a imagem parametrizada
+    #Define img como a imagem parametrizada
     img = Image.open(imagem)
     
-    
-    ##define proporções originais e novas a partir da porcentagem
+    #Define proporções originais e novas a partir da porcentagem
     largura_original, altura_original = img.size
 
-    
+    #Calcula a nova largura e altura a partir da porcentagem
+    #Obrigada Cauã e Miguel (mencionados no readme) pela ajuda com o cálculo
+    #A raiz quadrada é usada para manter a proporção da imagem, por isso, pode haver uma variação mínuscula de proporção entre as imagens
     largura = int(largura_original * (((100-porcentagem) / 100)**0.5))
     altura = int(altura_original * (((100-porcentagem) / 100)**0.5))
     
-    ##define um vetor com valores de comeco e fim para cortar a imagem, os calculando a partir da relacao das novas proporções com as originais, divindo por 2 para centralizar
+    #Cria um vetor com valores de comeco e fim para cortar a imagem, os calculando a partir da relacao das novas proporções com as originais, divindo por 2 para centralizar
     caixa = (((largura_original - largura) // 2), ((altura_original - altura) // 2), ((largura_original + largura) // 2), ((altura_original + altura) // 2))
     
-    ##define nova imagem a partir do crop da antiga 
+    #Obtem nova imagem a partir do crop da antiga com o vetor criado
     nova_img = img.crop(caixa)
     return nova_img
 
 def mainbdremove(dir,porcentagem):
-    ##define o diretorio e o valor da porcentagem a ser removida
+    #Define o diretorio e o valor da porcentagem a ser removida
     diretorio = f'{dir}/'
 
-    ##define cada imagem no diretório
+    #Define cada imagem no diretório
     arquivos_imagens = [f for f in os.listdir(diretorio) if f.endswith(('.jpg', '.png', '.jpeg', '.JPG', '.PNG', '.JPEG'))]
-    ##cria pasta output caso não exista
+    #Cria pasta output caso não exista
     os.makedirs('outputBD/', exist_ok=True)
 
-    ##executa o corte para cada imagem
+    #Executa o corte para cada imagem
     for imagem in arquivos_imagens:
         nv=removeborda(os.path.join(diretorio, imagem), (porcentagem))
-        ##salva a nova imagem na pasta output
+        #Salva a nova imagem na pasta output
         os.chdir('outputBD/')
         nv.save(f'{imagem}_{porcentagem}%borderremoved.jpg')
         os.chdir("..")
--- src/corte_lote.py
+++ src/corte_lote.py
@@ -2,21 +2,24 @@ import os
 from PIL import Image
 
 def cortar_imagem(imagem, largura, altura):
+    #Abre a imagem e pega informações para os cálculos
     img = Image.open(imagem)
     largura_original, altura_original = img.size
     quantidade_horizontal = largura_original // largura
     quantidade_vertical = altura_original // altura
 
+    #Cria um vetor para armazenar as imagens cortadas
     imagens_cortadas = []
 
+    #Corta a imagem em partes iguais, mantendo a resolução especificada até a quantidade possivel
     for y in range(quantidade_vertical):
         for x in range(quantidade_horizontal):
             box = (x * largura, y * altura, (x + 1) * largura, (y + 1) * altura)
             nova_imagem = img.crop(box)
             imagens_cortadas.append(nova_imagem)
 
-    # Se a imagem não for divisível perfeitamente pela resolução especificada,
-    # adicione o restante da imagem no lado direito e inferior
+    #Se a imagem não for divisível perfeitamente pela resolução especificada, adiciona o restante da imagem no lado direito e inferior
+    #Isso pode resultar em uma perda pequena do ponto inferior direito, onde não é possivel manter alguma das resoluções na pedida pelo usuário
     if largura_original % largura != 0:
         for y in range(quantidade_vertical):
             box = (quantidade_horizontal * largura, y * altura, largura_original, (y + 1) * altura)
@@ -33,20 +36,20 @@ def cortar_imagem(imagem, largura, altura):
 
 
 def maincorte (largura_resolucao, altura_resolucao):
-    # Diretório contendo as imagens
-    diretorio = 'outputBD/'  # Substitua 'imagens/' pelo caminho do diretório onde estão as imagens
+    #Diretório contendo as imagens com bordas removidas
+    diretorio = 'outputBD/'  
 
-    # Lista todos os arquivos de imagem no diretório
+    #Lista todos os arquivos de imagem no diretório
     arquivos_imagens = [f for f in os.listdir(diretorio) if f.endswith(('.jpg', '.png', '.jpeg', '.JPG', '.PNG', '.JPEG'))]
 
     os.makedirs('output/', exist_ok=True)
-    # Itera sobre cada imagem, corta e salva as partes cortadas
+    #Roda a função em cada imagem, corta e salva as partes cortadas
     for imagem in arquivos_imagens:
         imagens_cortadas = cortar_imagem(os.path.join(diretorio, imagem), largura_resolucao, altura_resolucao)
-        # Salvando as imagens cortadas
-        for i, img in enumerate(imagens_cortadas):
+        #Salvando as imagens cortadas
+        for i, img in enumerate (imagens_cortadas):
             os.chdir('output/')
-            img.save(f'imagem_cortada_{i}_{imagem}.jpg')
+            img.save(f'ic_{i}_{imagem}.jpg')
             os.chdir("..")
             
     print("Corte realizado com sucesso! :3")
