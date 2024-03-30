92a48162635514174d2ca864d1dd08beb4a4b89d
Estêvão Fiorilo <estevaofiorilo@gmail.com>
2024-03-30 20:00:47 -0300

    permite uso de pasta inicial de diferentes nomes

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ from src.corte_lote import maincorte
 from src.bdremove import mainbdremove
 import os
 
-##dirinicial=str(input("Digite o nome da pasta em que as fotos estão localizadas: ")) ##nao utilizado pois sera usada a pasta padrão input
+dirinicial=str(input("Digite o nome da pasta em que as fotos estão localizadas: ")) 
 porcentagem = int(input("Digite a porcentagem de borda que deseja remover: "))
 largura_resolucao = int(input("Digite a largura da resolução desejada: "))
 altura_resolucao = int(input("Digite a altura da resolução desejada: "))
 
-##mainbdremove(dirinicial,porcentagem) ##nao utilizado pois sera usada a pasta padrão input
-mainbdremove('input',porcentagem)
+mainbdremove(dirinicial,porcentagem)
 maincorte(largura_resolucao, altura_resolucao)
 for arq in os.listdir('outputBD'):
     os.remove(f'outputBD/{arq}')
